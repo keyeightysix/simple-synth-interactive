96a575a9c919af09ff11d052309f29bdf55eba93
KES <keyeightysix@flowethic.com>
2023-01-06 06:09:45 +0000

    Update package-lock.json and synth.tsx

--- src/lib/synth.tsx
+++ src/lib/synth.tsx
@@ -1,8 +1,10 @@
-import { el } from '@elemaudio/core';
-import { For, onMount } from 'solid-js';
-import { createStore } from 'solid-js/store';
+import { el } from "@elemaudio/core";
+import { For, onMount } from "solid-js";
+import { createStore } from "solid-js/store";
 
 export default function Synth(props: any) {
+  let context = {};
+  let ready = false;
   const core = props.core;
 
   const HzPerMinute = 60,
@@ -16,36 +18,38 @@ export default function Synth(props: any) {
     chord: [349.23, 440, 523.25],
     gate: gateInHz,
     amp: [
-      { id: 0, name: 'ampEnvA', value: 0.01 },
-      { id: 1, name: 'ampEnvD', value: 0.25 },
-      { id: 2, name: 'ampEnvS', value: 0.0 },
-      { id: 3, name: 'ampEnvR', value: 0.1 },
+      { id: 0, name: "ampEnvA", value: 0.01 },
+      { id: 1, name: "ampEnvD", value: 0.25 },
+      { id: 2, name: "ampEnvS", value: 0.0 },
+      { id: 3, name: "ampEnvR", value: 0.1 },
     ],
     filterEnv: [
-      { id: 0, name: 'filtEnvA', value: 0.01 },
-      { id: 1, name: 'filtEnvD', value: 0.1 },
-      { id: 2, name: 'filtEnvS', value: 0.0 },
-      { id: 3, name: 'filtEnvR', value: 0.1 },
+      { id: 0, name: "filtEnvA", value: 0.01 },
+      { id: 1, name: "filtEnvD", value: 0.1 },
+      { id: 2, name: "filtEnvS", value: 0.0 },
+      { id: 3, name: "filtEnvR", value: 0.1 },
     ],
     filter: [
-      { id: 0, name: 'intF', value: 4000 },
-      { id: 1, name: 'endF', value: 300 },
-      { id: 2, name: 'res', value: 0.5 },
+      { id: 0, name: "intF", value: 4000 },
+      { id: 1, name: "endF", value: 300 },
+      { id: 2, name: "res", value: 0.5 },
     ],
   };
 
   const [store, setStore] = createStore(patch);
 
-  const update = (keyValue) => {
+  const update = (keyValue: any) => {
     setStore(...keyValue);
-    console.log(store.amp);
-    render();
+    if (ready) {
+      render();
+    }
   };
 
-  let context = {};
-
   const playback = async (checked, action) => {
+    console.log(store);
+
     if (Object.keys(context).length === 0) {
+      console.log("creating new context");
       context = new window.AudioContext();
 
       let node = await core.initialize(context, {
@@ -55,15 +59,20 @@ export default function Synth(props: any) {
       });
 
       node.connect(context.destination);
+
+      ready = true;
     }
 
-    if (action === 'play' && checked) {
-      context.resume();
-      render();
+    if (action === "play" && checked) {
+      console.log(ready, context);
+      if (ready) {
+        context.resume();
+        render();
+      }
     }
 
-    if (action === 'stop' && checked) {
-      console.log('yo!!!!');
+    if (action === "stop" && checked) {
+      console.log("yo!!!!");
       context.suspend();
     }
   };
@@ -74,16 +83,23 @@ export default function Synth(props: any) {
     // setStore(param, value);
     console.log(store);
     console.log(core);
+    console.log(context);
+    //return false;
     // core.render(el.cycle(440));
     // return false;
 
-    const pulseTrain = el.train(gateInHz);
+    const pulseTrain = el.train(store.gate);
+    // core.render(pulseTrain);
+    // return false;
 
     const [aA, aD, aS, aR] = [...store.amp];
 
     console.log(aA.value, aD.value, aS.value, aR.value, pulseTrain);
 
     const env = el.adsr(aA.value, aD.value, aS.value, aR.value, pulseTrain);
+
+    // core.render(env);
+    // return false;
 
     const synthVoice = (env) =>
       el.mul(
@@ -95,47 +111,50 @@ export default function Synth(props: any) {
         )
       );
 
-    const test = el.lowpass(3000, 1, el.cycle(440));
-
-    console.log(core.render(test));
+    core.render(synthVoice(env));
     return false;
 
-    //core.render(synthVoice(env));
+    // --------------
+
+    const f = el.mul(0.25, el.lowpass(3000, 1, el.cycle(440)));
+    core.render(f, f);
+    return false;
+
+    const test2 = el.lowpass(
+      el.add(300, el.mul(10000, env)),
+      1,
+      synthVoice(env)
+    );
+
+    console.log(core.render(test2));
+    return false;
+
+    // --------------
 
     const [fA, fD, fS, fR] = [...store.filterEnv];
 
     console.log(fA.value, fD.value, fS.value, fR.value, pulseTrain);
-
     const fe = el.adsr(fA.value, fD.value, fS.value, fR.value, pulseTrain);
 
-    // const modulation = (theIntFreq, theFilterEnv) => {
-    //   return el.mul(theIntFreq, theFilterEnv);
-    // };
+    const modulation = (theIntFreq, theFilterEnv) =>
+      el.mul(theIntFreq, theFilterEnv);
 
-    // const filter = (intModFreq, filtEnv, FiltQ, filtFreq, theVoice) =>
-    //   el.lowpass(
-    //     el.add(filtFreq, el.mul(intModFreq, filtEnv)),
-    //     FiltQ,
-    //     theVoice
-    //   );
+    const filter = (intModFreq, filtEnv, FiltQ, filtFreq, theVoice) =>
+      el.lowpass(
+        el.add(filtFreq, el.mul(intModFreq, filtEnv)),
+        FiltQ,
+        theVoice
+      );
 
     const [intf, ef, q] = [...store.filter];
     console.log(intf.value, ef.value, q.value);
-    // const dry = el.mul(
-    //   0.25,
-    //   filter(intf.value, fe, q.value, ef.value, synthVoice(env))
-    // );
 
     const dry = el.mul(
       0.25,
-      el.lowpass(
-        el.add(ef.value, el.mul(intf.value, fe)),
-        q.value,
-        synthVoice(env)
-      )
+      filter(intf.value, fe, q, ef.value, synthVoice(env))
     );
 
-    console.log(core.render(test));
+    console.log(core.render(dry));
   };
 
   return (
@@ -148,7 +167,7 @@ export default function Synth(props: any) {
             name="playback-button"
             value="play"
             onChange={(e) => {
-              playback(e.currentTarget.checked, 'play');
+              playback(e.currentTarget.checked, "play");
             }}
           />
         </label>
@@ -159,7 +178,7 @@ export default function Synth(props: any) {
             name="playback-button"
             value="stop"
             onChange={(e) => {
-              playback(e.currentTarget.checked, 'stop');
+              playback(e.currentTarget.checked, "stop");
             }}
           />
         </label>
@@ -180,9 +199,9 @@ export default function Synth(props: any) {
                   value={param.value}
                   onInput={(e) => {
                     update([
-                      'amp',
+                      "amp",
                       param.id,
-                      'value',
+                      "value",
                       Number(e.currentTarget.value),
                     ]);
                   }}
@@ -208,9 +227,9 @@ export default function Synth(props: any) {
                   value={param.value}
                   onInput={(e) => {
                     update([
-                      'filter',
+                      "filter",
                       param.id,
-                      'value',
+                      "value",
                       Number(e.currentTarget.value),
                     ]);
                   }}
@@ -236,9 +255,9 @@ export default function Synth(props: any) {
                   value={param.value}
                   onInput={(e) => {
                     update([
-                      'filterEnv',
+                      "filterEnv",
                       param.id,
-                      'value',
+                      "value",
                       Number(e.currentTarget.value),
                     ]);
                   }}
